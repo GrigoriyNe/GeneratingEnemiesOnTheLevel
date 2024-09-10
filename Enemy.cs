d1234225755e8f6c9e07a7bfbc9b2f14b462497e
GrigoriyNe <159809163+GrigoriyNe@users.noreply.github.com>
2024-09-11 01:01:41 +0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] public float _speed;
 
     private Renderer _renderer;
     private Rigidbody _rigidbody;
-    private Vector3 _finishPosition;
+    private Vector3 _randomDirection;
 
     private void Awake()
     {
@@ -14,23 +16,22 @@ public class Enemy : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    public void Init(Vector3 startPosition, Quaternion rotation, Color color, Vector3 rigidbody, Vector3 finishPosition)
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Init(Vector3 startPosition, Quaternion rotation, Color color, Vector3 rigidbody, Vector3 randomDirection)
     {
         transform.position = startPosition;
         transform.rotation = rotation;
         _renderer.material.color = color;
         _rigidbody.velocity = rigidbody;
-        _finishPosition = finishPosition;
+        _randomDirection = randomDirection;
     }
 
-    private void Update()
+    private void Move()
     {
-        Movement();
-    }
-
-    private void Movement()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, _finishPosition, _speed * Time.deltaTime);
+        transform.Translate(_randomDirection * _speed * Time.fixedDeltaTime);
     }
 }
-
